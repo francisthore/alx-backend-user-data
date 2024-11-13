9b18dd4f8294e4d6458ea438f53bd2ed3ff4d162
Francis Thore <thorefrancis@gmail.com>
2024-11-14 00:33:47 +0200

    create session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,12 +3,20 @@
     Module that implements session authentication mechanism
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Class that implements session authentication system
     """
+    user_id_by_session_id = {}
 
-    def __init__(self) -> None:
-        """Initialises the class objects"""
-        super().__init__()
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session id for a user with user_id"""
+        if not user_id:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
