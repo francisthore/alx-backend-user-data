0b626acd9ddd6e6ae43dd2095c3bff951fc04a88
Francis Thore <thorefrancis@gmail.com>
2024-11-21 21:38:48 +0200

    echo false

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,13 +2,14 @@
 """
 Main file
 """
+
+from db import DB
 from user import User
-from sqlalchemy import create_engine
 
+my_db = DB()
 
+user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+print(user_1.id)
 
-
-print(User.__tablename__)
-
-for column in User.__table__.columns:
-    print("{}: {}".format(column, column.type))
+user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+print(user_2.id)
